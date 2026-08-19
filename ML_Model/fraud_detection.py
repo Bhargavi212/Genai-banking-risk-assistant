eeddabf87e59ab528654dc3b61faab0b3183cbde
Bhargavi212 <alumolub@gmail.com>
2026-08-19 12:23:57 -0500

    Use local MLflow tracking for portable training

--- ML_Model/fraud_detection.py
+++ ML_Model/fraud_detection.py
@@ -58,7 +58,7 @@ N_ESTIMATORS = 200
 # MLflow
 # ---------------------------------------------------------
 
-mlflow.set_tracking_uri("http://localhost:5001")
+mlflow.set_tracking_uri("file:./mlruns")
 mlflow.set_experiment("fraud_risk")
 
 
